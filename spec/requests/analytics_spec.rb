9079d29e2a857b34432d1e00fc4cb677fa373634
collinsugwu <collinsuchinaka@gmail.com>
2021-12-15 12:55:51 +0100

    test analyticx

--- spec/requests/analytics_spec.rb
+++ spec/requests/analytics_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Analytics", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  # Initialize the test data
+  let!(:survey_detail) { create(:survey_detail) }
+  let(:survey_id) { survey_detail.id }
+  let!(:question) { create(:question, survey_detail_id: survey_detail.id) }
+  let!(:options) { create_list(:option, 5, question_id: question.id) }
+  let(:question_id) { question.id }
+  let(:id) { options.first.id }
+  let!(:answer) {create(:answer, question_id: question.id, option_id: options.first.id)}
+
+
+  # Test suite for GET /survey/:id/analytics
+  describe "GET /survey/:id/analytics" do
+    before { get "/surveys/#{survey_id}/analytics" }
+
+    context 'when question options exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
   end
 end
